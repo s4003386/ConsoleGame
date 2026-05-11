15fde1271917c2f5e457954b4119c8c754303380
s4003386 <s4003386@student.rmit.edu.au>
2026-05-11 14:10:48 +1000

    i

---
+++ src/main.java
@@ -0,0 +1,10 @@
+package src;
+
+public class main {
+
+    //whatever classes go up here. 
+
+    public static void main(String[] args){
+        System.out.println("main");
+     } 
+}
---
+++ test/Navtest.java
@@ -0,0 +1,5 @@
+package test;
+
+public class Navtest {
+    //map navigation testing
+}
