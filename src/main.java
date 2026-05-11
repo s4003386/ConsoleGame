56440d2c713ca61bc986f6e2abb1d5fd75abeceb
s4003386 <s4003386@student.rmit.edu.au>
2026-05-11 15:03:17 +1000

    i

--- src/main.java
+++ src/main.java
@@ -4,6 +4,9 @@ public class main {
 
     //whatever classes go up here. 
 
+
+    //have a listener maybe where if a certain button is pressed, have the option to turn on console mode for testing
+    
     public static void main(String[] args){
         System.out.println("main");
      } 
